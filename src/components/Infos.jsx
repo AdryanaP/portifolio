13440b91df3a272548f955778ec530d5e70d57ef
Adryana Portugal <dryportuga@gmail.com>
2021-11-16 10:00:40 -0300

    componentes

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -1,22 +1,26 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
 export default function Infos() {
   return (
     <div className="p-4 md:flex md:justify-center gap-48 md:content-center md:justify-items-center h-screen pt-20">
-    <div className="space-y-8 md:max-w-30 md:self-center">
-      <div className="space-y-2">
-        <h2 className="text-4xl md:text-8xl md:text-left text-center">
-          Adryana
-        </h2>
-        <h2 className="text-4xl md:text-8xl md:text-right text-center">
-          Portugal
-        </h2>
+      <div className="space-y-8 md:max-w-30 md:self-center">
+        <a
+          href="https://github.com/AdryanaP"
+          className="flex gap-6 items-center"
+          target="_blank"
+        >
+          <BsGithub className="w-24 h-24" />
+          <p>/AdryanaP</p>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/adryanaportugal/"
+          className="flex gap-6 items-center"
+          target="_blank"
+        >
+          <BsLinkedin className="w-24 h-24" />
+          <p>/adryanaportugal</p>
+        </a>
       </div>
-      <p className="">
-        Sou uma Desenvolvedora apaixonada por Front-End e descobri na
-        programação minha verdadeira vocação. Prezo por criar layouts
-        responsivos, acessíveis e bonitos, de forma que valorize a experiencia
-        do usuário e o proporcione-o uma navegação intuitiva.
-      </p>
     </div>
-  </div>
-  )
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,43 @@
 export default function Projects() {
+  const projects = [
+    {
+      id: 1,
+      title: "Twitter com NuxtJs e Vue",
+      link: "https://github.com/AdryanaP/twitter-nuxtjs",
+    },
+    {
+      id: 2,
+      title: "Tip Calculator",
+      link: "https://github.com/AdryanaP/tip-calculator",
+    },
+    {
+      id: 3,
+      title: "Google",
+      link: "https://github.com/AdryanaP/google",
+    },
+    {
+      id: 4,
+      title: "Estação do Café",
+      link: "https://github.com/VinnyMendes/EstacaoCafe",
+    },
+  ];
+
   return (
     <div className="p-4 md:flex md:justify-center gap-48 md:content-center md:justify-items-center h-screen pt-20">
       <div className="space-y-8 md:max-w-30 md:self-center">
-        <ul>
-          <li>
-            <p>Twitter com NuxtJs e Vue</p>
-            <a href="https://github.com/AdryanaP/twitter-nuxtjs">Link</a>
-          </li>
+        <ul className="grid grid-cols-2 gap-4 justify-center justify-items-center">
+          {projects.map((item) => {
+            return (
+              <li className="text-center" key={item.id}>
+                <p>{item.title}</p>
+                <a href={item.link} target="_blank">
+                  Link
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
-  )
+  );
 }
